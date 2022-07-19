d26bb55796a7815336bd385e311a68ff345e9598
Anton <kramarenko4362@gmail.com>
2022-07-19 17:25:33 +0300

    add page title

--- src/components/componentsPage/PageHeader/PageHeader.tsx
+++ src/components/componentsPage/PageHeader/PageHeader.tsx
@@ -1,9 +1,51 @@
 import React  from 'react'
+import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import './PageHeader.scss'
+import ExamsIMG from '../../../assets/icons/Frame.svg'
+import RejectImg from '../../../assets/icons/reject.png'
+import ApproveImg from '../../../assets/icons/approve.png'
 
-export const PageHeader = () => {
+
+interface IPageHeader{
+  title: string,
+  titleReject: string,
+  titleApprove:string,
+  clickReject: () => void,
+  clickApprove: () => void,
+}
+
+export const PageHeader: React.FC<IPageHeader> = ({title, clickReject, clickApprove,titleReject,titleApprove}) => {
+
+  const currentLoacation= useLocation();
+
+  // must refactoring
+const location  = () => {
+  const  locationArr = currentLoacation.pathname.split('/').filter(location => location)
+
+  return  locationArr.map((item, index,arr) => {
+      if(arr.length-1 === index ){
+        return <span key={item} className='pageHeader__location-item'>{item}</span>
+      } else {
+        return <React.Fragment key={item} ><span className='pageHeader__location-item'>{item}</span><span>{'>'}</span></React.Fragment>
+      }
+  })
+}
   return (
-    <div className='pageHeader'></div>
+    <section className='pageHeader'>
+      <div className="pageHeader__info">
+        <div className="pageHeader__info-location">  <img src={ExamsIMG} alt="" />{location()}</div>
+       
+        <h1 className="pageHeader__info-title">{title}</h1>
+      </div>
+      <div className="pageHeader__actions">
+        <button className='pageHeader__actions-btn'>
+          <img src={RejectImg} alt="" />
+          {titleReject}</button>
+        <button className='pageHeader__actions-btn active'>
+        <img src={ApproveImg} alt="" />
+          {titleApprove}</button>
+      </div>
+    </section>
   )
 }
--- src/pages/NewExam/NewExam.tsx
+++ src/pages/NewExam/NewExam.tsx
@@ -6,7 +6,13 @@ export const NewExam = () => {
     
   return (
     <div className='newExam'>
-        <PageHeader/>
+        <PageHeader 
+          title='New Exam Title Here' 
+          clickReject={console.log} 
+          clickApprove={console.log} 
+          titleReject='Delete Exam' 
+          titleApprove='Save Exam'
+        />
         
         <span>NewExam</span>
         
